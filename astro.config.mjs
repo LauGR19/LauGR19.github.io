df1b2d40194ca4e7127678b6396a37a22cc84d2a
LauGR19 <ljguerreror@unbosque.edu.co>
2026-02-17 18:55:17 -0500

    Update site URL and output format in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  
+  site: 'https://laugr19.github.io/',
+  output: 'static',
   integrations: [tailwind()],
 });
